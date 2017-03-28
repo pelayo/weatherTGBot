6329648c26958b9edbb07f4f68e39eedf7e68a62
Pelayo Ramón <pelayo.ramon@clouddistrict.com>
2017-03-28 18:41:07 +0200

    added mongo connection

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 const TelegramBot = require('node-telegram-bot-api');
 const argv = require('minimist')(process.argv.slice(2))
 const _ = require('lodash')
+const MongoClient = require('mongodb').MongoClient
+
+
+
 // replace the value below with the Telegram token you receive from @BotFather
 const token = argv._[0]
+const mongoUrl = 'mongodb://localhost:27017/tgWeather'
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
+const attachBot = (bot, db) => {
+  bot.onText(/\/warning (over|under) (-|\+\d+) (\d+) (hours|days)/, (msg, match) => {
+    const chatId = msg.chat.id;
+    console.log(match)
+    const resp = match[1]; // the captured "whateveir"
+    db.collection('alarms').insert({
+      msg,
+      type: match[1],
+      temp: match[2],
+      time: match[3],
+      timeUnit: match[4]
+    })
+    bot.sendMessage(chatId, resp);
+  });
+}
 
-// Matches "/echo [whatever]"
-bot.onText(/\/warning (over|under) (-|\+\d+) (\d+) (hours|days)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
-  const chatId = msg.chat.id;
-  console.log(match)
-  const resp = match[1]; // the captured "whateveir"
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
-});
+MongoClient.connect(mongoUrl, (err, db) => {
+  attachBot(bot, db)
+})
